5412faa890997f8edd7620a9d7ade042ab538e0b
Wesley Jacques <wesleyjacques@hotmail.com>
2022-03-27 09:25:46 -0300

    ::up:: Finalizando divisao da pagina

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-
+      <header></header>
+      <div className='aside'></div>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
