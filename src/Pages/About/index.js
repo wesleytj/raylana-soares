e332a2fd9088d1959cee77d580d7560456ac817c
Wesley Jacques <wesleyjacques@hotmail.com>
2022-03-28 06:55:06 -0300

    :up: about page

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -4,8 +4,26 @@ import './style.scss';
 
 function About() {
   return (
-    <div container>
-        <h2>Aqui é a página sobre</h2>
+    <div className='container'>
+      <div className='about-wrapper profile-wrapper'>
+        <div className='profile'></div>
+        <div className='profile-txt'>
+          <p>Raylana Soares</p>
+          <div>
+            <p>@raylananaildesign</p>
+            <p>(51) 9 9647-1743</p>
+          </div>
+        </div>
+      </div>
+      <div className='about-wrapper description-wrapper'>
+        <p>Oi, eu sou a Ray ;) !</p>
+        <div>
+          <p>Formação:</p>
+          <div></div>
+          <div></div>
+        </div>
+
+      </div>
     </div>
   );
 }
