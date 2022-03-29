a2f4070fba5d4818d221809ca373761f4af880b9
Wesley Jacques <wesleyjacques@hotmail.com>
2022-03-28 21:08:54 -0300

    :up: about page and :up: responsive about page

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -4,7 +4,7 @@ import './style.scss';
 
 function About() {
   return (
-    <div className='container'>
+    <div className='container container-about'>
       <div className='about-wrapper profile-wrapper'>
         <div className='profile'></div>
         <div className='profile-txt'>
@@ -12,15 +12,21 @@ function About() {
           <div>
             <p>@raylananaildesign</p>
             <p>(51) 9 9647-1743</p>
+            <p>Rua Demétrio Ribeiro, 240 <br />
+            93110-240 São Leopoldo - RS</p>
           </div>
         </div>
       </div>
       <div className='about-wrapper description-wrapper'>
         <p>Oi, eu sou a Ray ;) !</p>
         <div>
-          <p>Formação:</p>
+          <p>Formações:</p>
+          {/* 
+           //Aqui vai a foto dos certificados
           <div></div>
-          <div></div>
+          <div></div> */}
+          <p>Alongamento acrílico para iniciante - <span>Renata Queiroz</span></p>
+          <p>Aperfeiçoamento de acrílico no molde - <span>Verônica Buzellato</span></p>
         </div>
 
       </div>
